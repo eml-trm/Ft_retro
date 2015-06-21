66ecc3622cd96db7dd38b7467a2ea30c1684fe64
etermeau <etermeau@student.42.fr>
2015-06-21 22:05:37 +0200

    dshh

---
+++ srcs/BossMissile.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BossMissile.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etermeau <etermeau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/06/21 22:02:21 by etermeau          #+#    #+#             */
+/*   Updated: 2015/06/21 22:02:22 by etermeau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BossMissile.class.hpp"
+#include "Missile.class.hpp"
+
+/* Constructor */
+BossMissile::BossMissile( int X, int Y): Missile("Boss Missile", 100, X, Y, 4, 3, "0000") {
+//		std::cout << "Boss missile was created" << std::endl;
+		return ;
+}
+
+
+/* Destructor */
+BossMissile::~BossMissile( void ) {
+//	std::cout << "Boss missile was destroyed" << std::endl;
+	return ;
+}
+
+/* Operator */
+
+
+/* Get */
+
+
